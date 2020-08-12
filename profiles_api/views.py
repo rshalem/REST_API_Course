9dd0d7332d2fd1907751001bc025c2c8ae61b9f4
rshalem <shalem000@gmail.com>
2020-08-12 22:36:37 +0530

    Added login API, permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from . import serializers
 from .models import UserProfile
@@ -13,6 +15,7 @@ from .permissions import UpdateOwnProfile
 class UserProfileViewSet(ModelViewSet):
     """Handle creating & updating profiles"""
     """Handles all HTTP requests, via methods ie list, create,etc """
+    """Authentication checks for a token in request header, if so then allows the request to go through given permission classes"""
 
     serializer_class = serializers.UserProfileSerializer
     # this objects manager is UserProfileManager that we created
@@ -21,3 +24,7 @@ class UserProfileViewSet(ModelViewSet):
     permission_classes = (UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginAPIView(ObtainAuthToken):
+    """Handle creating user authentication tokens, displaying login"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
